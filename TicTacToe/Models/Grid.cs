e7555be8eac2b14339f6f3513f2d4288224bc9e3
sutrkiller <tobias.kamenicky@gmail.com>
2016-11-25 11:00:42 +0100

    Add project files.

---
+++ TicTacToe/Models/Grid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Models
+{
+    public class Grid
+    {
+        private readonly int _size;
+        private readonly int[][] _grid;
+        public Grid(int size)
+        {
+            _size = size;
+            _grid = new int[_size][];
+            for (var i = 0; i < _size; i++)
+            {
+                _grid[i] = new int[_size];
+            }
+        }
+    }
+}
---
+++ TicTacToe/Models/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Models
+{
+    public class Node
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Value { get; set; }
+        public int Max { get; set; } //Beta
+        public int Min { get; set; } //Alpha
+        public bool IsMaxNode { get; set; }
+        public IList<Node> Children { get; set; }
+    }
+}
